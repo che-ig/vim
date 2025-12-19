2bf7f302caed8ec459e977c07612b9a629fdabdb
chernov.ig <mr.ivan.chernov@yandex.ru>
2025-12-19 16:08:38 +0300

    добавил описания к командам

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,23 +9,36 @@ local builtin = require('telescope.builtin')
 local lga_actions = require("telescope-live-grep-args.actions")
 local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Find files"})
-vim.keymap.set('n', '<leader>fw', builtin.live_grep, {desc = "Search for text"})
+vim.keymap.set('n', '<leader>ff', builtin.find_files,
+               {desc = "Telescope find files"})
+vim.keymap.set('n', '<leader>fw', builtin.live_grep,
+               {desc = "Telescope search for text"})
 
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Find buffers"})
-vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics,
+               {desc = 'Telescope diagnostics'})
 vim.keymap.set('n', '<leader>fn', builtin.resume, {})
 -- vim.keymap.set('n', '<Tab>', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = "Help tags"})
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
-vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
-vim.keymap.set('n', 'gr', builtin.lsp_references,
-               {noremap = true, silent = true})
-vim.keymap.set('n', 'gd', builtin.lsp_definitions,
-               {noremap = true, silent = true})
-vim.keymap.set("n", ",f",
+vim.keymap.set('n', '<leader>gb', builtin.git_branches,
+               {desc = 'Telescope git branches'})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits,
+               {desc = 'Telescope git commits'})
+vim.keymap.set('n', '<leader>gs', builtin.git_status,
+               {desc = 'Telescope git status'})
+vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols,
+               {desc = "Telescope show document symbols"})
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {
+    noremap = true,
+    silent = true,
+    desc = 'Telescope LSP references'
+})
+vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {
+    noremap = true,
+    silent = true,
+    desc = "Jumps to the definition of the symbol under the cursor"
+})
+vim.keymap.set("n", ",ff",
                ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
                {desc = "Seaarch for text in files"})
 vim.keymap.set("n", ",fc", live_grep_args_shortcuts.grep_word_under_cursor,
